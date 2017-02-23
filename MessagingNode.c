eaaad70a8b3b38d11537486b960e4415127dfabc
darren-2016 <darren.draper@gpsengine.net>
2017-02-23 22:19:00 +1000

    Building structure to code.

--- MessagingNode.c
+++ MessagingNode.c
@@ -1,8 +1,70 @@
+
+#include <stdlib.h>
 #include <stdio.h>
 
+
+/************************************************************
+ * Function:    modemSend
+ * Description: 
+ * Returns:     void
+ */
+void modemSend(char *message)
+{
+    printf("> %s\n", message);
+}
+
+/************************************************************
+ * Function:    WorkerPending
+ * Description: 
+ * Returns:     void
+ */
+void WorkerPending()
+{
+    printf("WorkerPending\n");
+
+    modemSend("WorkerPending");
+    
+}
+
+/************************************************************
+ * Function:    WorkerComplete
+ * Description: 
+ * Returns:     void
+ */
+void WorkerComplete()
+{
+    printf("WorkerComplete\n");
+
+    modemSend("WorkerComplete");
+}
+
+/************************************************************
+ * Function:    WorkerReceived
+ * Description: 
+ * Returns:     void
+ */
+void WorkerReceived()
+{
+    printf("WorkerReceived\n");
+
+    modemSend("WorkerReceived");
+}
+
+/************************************************************
+ * Function:    main
+ * Description: Main entry point.
+ * Returns:     void
+ */
 int main()
 {
     printf ("Messaging Node\n");
-    
+
+    WorkerPending();
+
+    WorkerReceived();
+
+    WorkerComplete();
+
+    return EXIT_SUCCESS;
 
 }
